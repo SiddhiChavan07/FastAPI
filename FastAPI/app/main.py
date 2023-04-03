205d38668ce22002bdffa96f7b0db39cba5188bc
gaurihpatil <gauripatil22001@gmail.com>
2023-04-03 22:22:12 +0530

    API

---
+++ FastAPI/app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional, List
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from . import models,schemas, utils
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='root', cursor_factory=RealDictCursor)
+        cursor= conn.cursor()
+        print("Database connection was successfull")
+        break
+    except Exception as error:
+        print("Connecting to database failed")  
+        print("Error: ", error)   
+        time.sleep(2)
+    
+
+my_posts =  [{"title": "title of post 1","content":"content of post 1","id":1},{"title":"favorite foods","content":"I like pizza", "id":2}]    
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p 
+        
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i 
+    
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
